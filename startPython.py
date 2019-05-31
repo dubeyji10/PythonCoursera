351b95832902e26086f82b4b0708e6dc506161d7
Abhishek Dubey <45308951+dubeyji10@users.noreply.github.com>
2019-05-31 11:22:33 +0530

    Add files via upload

---
+++ startPython.py
@@ -0,0 +1,35 @@
+def hello():
+    print("Hello, world!")
+
+hello()
+
+#---------------------------------------
+
+
+#%%
+def myname():
+    print("My name is Bill")
+
+myname()
+
+#-----------------------------------------
+
+#%%
+def ourschool():
+    print("Coursera is our school")
+
+ourschool()
+
+#-----------------------------------------
+
+""" This will run forever. In this case Ctrl-C will stop it, but sometimes
+that doesn't work. In that case, click away IP Console to stop; click yes to 
+kill kernel. Open a new IPConsole on the Console menu to restart """
+#%%
+def forever():
+    while True:
+        print("still true stop the process")
+        pass
+
+forever()
+#%%
